355ac602e61b82d5254c3670909439ea9d71dc22
pbcs.ghaustein <gerry.haustein@pbcsolutions.ca>
2025-03-31 09:06:58 -0700

    minor

--- reminder.py
+++ reminder.py
@@ -1,26 +1,38 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+# from tkinter import ttk, messagebox
 import argparse
 import time
 import threading
 
-''''
-root.title("TK v1.09")
-root.geometry("220x92")
-root.configure(bg="#AAAADE") # rgb'
-'''
+root = None
+stop_reminder = False
+
+def disable_maximize(event = None):
+    global root
+    root.state('normal')
 
 class ReminderApp:
-    def __init__(self, message="Reminder!", minutes=30):
+    def __init__(self, message = "Reminder!", minutes = 30):
         self.message = message
         self.minutes = minutes
         self.root = tk.Tk()
+        root = self.root
         self.root.attributes("-topmost", True)
         self.root.configure(bg="#AAAADE") # rgb'
+        self.root.bind('<Map>', disable_maximize)
         self.root.withdraw()  # Hide the main window
+        # self.root.geometry("220x92")
 
     def show_reminder(self):
-        messagebox.showinfo("Reminder", self.message)
+        # Create a toplevel window
+        print(f"it's time")
+        popup = tk.Toplevel(self.root)
+        popup.title("Reminder")
+        popup.configure(bg="#AAAADE")
+
+        # Add a label with the message
+        label = tk.Label(popup, text=self.message, padx=20, pady=10, bg="#AAAADE")
+        label.pack()
 
     def run(self):
         while True:
